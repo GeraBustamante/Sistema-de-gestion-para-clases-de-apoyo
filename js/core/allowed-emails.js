adab31573569f19de9cb185c6182cec7e2bc9835
Gera Bustamante <gebp.6064@gmail.com>
2025-07-17 15:11:33 -0300

    mas correos para probar

--- js/core/allowed-emails.js
+++ js/core/allowed-emails.js
@@ -1,4 +1,6 @@
 // Lista de correos autorizados para acceder a la app
 export const allowedEmails = [
-  "gebp.6064@gmail.com", "claudiotorres375@gmail.com"
+  "gebp.6064@gmail.com", 
+  "claudiotorres375@gmail.com",
+  "nu.geera@gmail.com"
 ];
